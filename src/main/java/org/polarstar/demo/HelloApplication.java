c0e3e8150c653e5a545da06c581ff99eb259bebb
marinagumeniuc <marina.gumeniuk.ani@gmail.com>
2024-03-19 16:37:41 +0200

    Initial commit

---
+++ src/main/java/org/polarstar/demo/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.polarstar.demo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
